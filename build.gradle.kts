74abace6c339941849b732e4cdf627b96dd0a750
Olivier Marteaux <olivier_marteaux@hotmail.com>
2026-03-04 21:48:04 +0100

    40_20260304_OM: Release v9.0.7: updated AGP, Hilt and ksp versions to allow use of shared lib locally with project A056

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+//    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
     // for Hilt DI
@@ -12,5 +12,5 @@ plugins {
 }
 
 allprojects {
-    group = "com.github.oliviermarteaux"
+    group = "com.github.OlivierMarteaux.shared-android-library"
 }
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+//    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("maven-publish")
     // Add the dependency for the Google services Gradle plugin for Firebase authentication
