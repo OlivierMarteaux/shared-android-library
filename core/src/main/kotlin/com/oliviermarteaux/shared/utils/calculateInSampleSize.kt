9eed821fa30bc9fb5b094c40ad9a172ce01f8ee9
Olivier Marteaux <olivier_marteaux@hotmail.com>
2026-04-07 17:21:43 +0200

    48_20260407_OM: Release v10.0.3: Updated calculateInSampleSize

--- core/src/main/kotlin/com/oliviermarteaux/shared/utils/calculateInSampleSize.kt
+++ core/src/main/kotlin/com/oliviermarteaux/shared/utils/calculateInSampleSize.kt
@@ -8,9 +8,10 @@ fun calculateInSampleSize(context: Context): Int {
     Log.i("OM_TAG", "calculateInSampleSize: memoryClass = $memoryClass")
 
     return when {
-        memoryClass >= 512 -> 1   // High-end devices
-        memoryClass >= 256 -> 1   // Xiaomi Mi 11
-        memoryClass >= 128 -> 4
-        else -> 8                // Low-end devices
+        memoryClass >= 512 -> 1  // High-end devices - RedmiNote10: 512MB
+        memoryClass >= 256 -> 1  // Xiaomi Mi 11: 256MB
+        memoryClass > 192 -> 1
+        memoryClass > 128 -> 2   // Honor 5C: 192MB - Low-end devices
+        else -> 2                // Low-end devices
     }
 }
